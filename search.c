4d9077760250f370ab5b46f590e8a5b7bb7d3411
yanisnmn95130 <yanisnmn@gmail.com>
2025-05-04 17:20:08 +0200

    Create search.c

---
+++ search.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "animal.h"
+
+int chercheNom(Animal liste[], int taille, char* tabAnimal) {
+    for (int i = 0; i < taille; i++) {
+        if (/////////////) {
+            return 1; 
+        }
+    }
+    return 0;
+}
+
+int chercheID(Animal liste[], int taille, int id) {
+    for (int i = 0; i < taille; i++) {
+        if (liste[i].id == id) {
+            return i;
+        }
+    }
+    return 0;
+}
