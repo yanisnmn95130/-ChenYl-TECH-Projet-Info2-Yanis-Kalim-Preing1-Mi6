1067e4497154cccafc83b21fd90e7d1218397bd9
manizerr <kalim.chakeur@gmail.com>
2025-05-04 18:35:27 +0200

    maj input text

--- src/animal.h
+++ src/animal.h
@@ -1,6 +1,17 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
+void inputText(const char *prompt, char *buffer, int bufferSize) {
+    printf("%s: ", prompt);
+    fgets(buffer, bufferSize, stdin);
+
+    // Supprimer le caractère de nouvelle ligne (\n) si présent
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
+}
+
 
 typedef struct {
     int id ;
