5834961aad6eb0e420e4ce5985e4b8b44d7e706c
yanisnmn95130 <yanisnmn@gmail.com>
2025-05-08 21:41:54 +0200

    fichier.c  w commentaire

--- src/fichier.c
+++ src/fichier.c
@@ -4,34 +4,35 @@
 
 int saveAnimal(Animal a)
 {
-    FILE *file = fopen("animals.txt", "a"); //"a"	Ajout (append) à la fin du fichier, sans supprimer son contenu.
+    FILE *file = fopen("animals.txt", "a");//fopen("animals.txt", "a") ouvre le fichier animals.txt ,"a" Ajout à la fin du fichier, sans supprimer son contenu.
+    //FILE= gere les fichier, *file = pointeur accede au fichier ouvert
     if (file == NULL)
     {
         printf("Erreur lors de l'ouverture du fichier");
         return 0;
     }
 
-    if (fprintf(file, "%d;%s;%s;%d;%.2f;%s\n", a.id, a.name, a.species, a.birthYear, a.weight, a.comment) < 0)
+    if (fprintf(file, "%d;%s;%s;%d;%.2f;%s\n", a.id, a.name, a.species, a.birthYear, a.weight, a.comment) < 0)// fprintf = ecrit des infos dans un fichier
     {
-        fclose(file);
+        fclose(file);// ferme le fichier OBLIGATOIRE a la fin
         printf("Erreur lors de l'écriture du fichier");
-        return 0;
+        return 0;// sauvegarde rater, return 0
     }
 
     fclose(file);
-    return 1;
+    return 1;// reussi donc return 1
 }
 
 int loadAnimals(Animal *animals, int maxSize)
 {
-    int NumberAnimal = 0;
-    int count = 0;
+    int NumberAnimal = 0;//nbr a charger
+    int count = 0;//nbr charger
 
-    // Demande le nombre d'animaux à charger.
+    
     printf("Combien d'animaux voulez-vous charger (max %d) ? ", maxSize);
-    if (scanf("%d", &NumberAnimal) != 1)
+    if (scanf("%d", &NumberAnimal) != 1)//Si scanf() ne renvoie pas 1, erreru dcp ca retourne 0
     {
-        fprintf(stderr, "Erreur de lecture du nombre d'animaux.\n"); // mesage errur dans le fichier
+        fprintf(stderr, "Erreur de lecture du nombre d'animaux.\n"); // fprint ecerit dans lefichier ,, stderr ecrit un mess derreur
         return 0;
     }
 
@@ -42,7 +43,7 @@ int loadAnimals(Animal *animals, int maxSize)
     }
 
     // Boucle de chargement des animaux.
-    for (int i = 0; i < NumberAnimal; i++)
+    /*for (int i = 0; i < NumberAnimal; i++)
     {
         printf("\nAnimal %d :\n", i + 1);
 
@@ -69,19 +70,20 @@ int loadAnimals(Animal *animals, int maxSize)
     }
     return count;
 }
-
+*/
 int countAnimals(const char *filename)
 {
-    FILE *file = fopen(filename, "r");
-    if (!file)
+    FILE *file = fopen(filename, "r");//ouvre filename en mode lecture "r"
+    if (file == NULL)
     {
         printf("Erreur : impossible d'ouvrir le fichier.\n");
-        return -1;
+        return 0;
     }
 
     int count = 0;
     Animal a;
-    while (fscanf(file, "%d %49s %29s %d %f %249s", &a.id, a.name, a.species, &a.birthYear, &a.weight, a.comment) == 6)
+    while (fscanf(file, "%d %49s %29s %d %f %249s", &a.id, a.name, a.species, &a.birthYear, &a.weight, a.comment) == 6)//fscanf() lit chaque ligne du fichier et extrait les infos de l'animal 
+    //boucle continue tant que fscanf lit 6 valeur et Chaque ligne valide augmente count de +1
     {
         count++;
     }
@@ -94,12 +96,12 @@ int deleteAnimal(Animal *animals, int *size, int id)
 {
     for (int i = 0; i < *size; i++)
     {
-        if (animals[i].id == id)
+        if (animals[i].id == id)//Compare l’ID de chaque animal avec celui donner . 
         {
-            animals[i] = animals[*size - 1];
+            animals[i] = animals[*size - 1]; // sil’ID correspond, l’animal trouvé  delete.
             (*size)--;
-            return 1;
+            return 1;//succes return 1
         }
     }
-    return 0;
+    return 0;// echec retourn 0
 }
