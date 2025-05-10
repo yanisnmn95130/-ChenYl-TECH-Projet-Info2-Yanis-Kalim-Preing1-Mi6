bb0e101b342fe46ba2e1f837105361a73f7237af
yanisnmn95130 <yanisnmn@gmail.com>
2025-05-10 11:46:47 +0200

    Update search.c

--- src/search.c
+++ src/search.c
@@ -2,16 +2,14 @@
 #include <string.h>
 #include "animal.h"
 
-int chercheNom(Animal liste[], int taille, char* nomAnimal) {
+int chercheNom(Animal liste[], int taille, char* tabAnimal) {
     for (int i = 0; i < taille; i++) {
-        if ( strcmp(liste[i].name, nomAnimal) == 0) {
-            return i; 
+        if (/////////////) {
+            return 1; 
         }
     }
-    return -1;
+    return 0;
 }
-
-
 
 int chercheID(Animal liste[], int taille, int id) {
     for (int i = 0; i < taille; i++) {
@@ -19,5 +17,32 @@ int chercheID(Animal liste[], int taille, int id) {
             return i;
         }
     }
+    return 0;
+}
+
+int chercheSpecies(Animal liste[], int taille, char* specie) {
+    for (int i = 0; i < taille; i++) {
+        if (strcmp(liste[i].species,  specie) == 0) {
+            printAnimal(liste[i]);
+        }
+    }
+    return 0;
+}
+
+int chercheAge(Animal liste[], int taille, char* age) {
+    for (int i = 0; i < taille; i++) {
+        if (strcmp(age, "jeune") == 0) {
+            if(calculateage(liste[i].birthYear) < 2){
+                printAnimal(liste[i]);
+            }
+        }
+        if (strcmp(age, "senior") == 0){
+            if (calculateage(liste[i].birthYear) > 10) {
+                printAnimal(liste[i]);
+            }
+            
+        }
+    }
+    
     return 0;
 }
