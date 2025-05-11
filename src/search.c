ccfff272e4a266294f3bf60a84df964eb56be8ee
yanisnmn95130 <yanisnmn@gmail.com>
2025-05-11 19:44:32 +0200

    finition rapide

--- src/search.c
+++ src/search.c
@@ -5,30 +5,36 @@
 
 int chercheNom(Animal liste[], int taille, char *nomAnimal)
 {
+    int trouve = 0;
     for (int i = 0; i < taille; i++)
     {
         if (strcmp(liste[i].nom, nomAnimal) == 0)
         {
             afficherAnimal(liste[i]);
+            trouve += 1;    
         }
+
     }
-    return 0;
+    return trouve;
 }
 
-int chercheEspece(Animal liste[], int taille, char *specie)
+int chercheEspece(Animal liste[], int taille, char *espece)
 {
+    int trouve = 0;
     for (int i = 0; i < taille; i++)
     {
-        if (strcmp(liste[i].espece, specie) == 0)
+        if (strcmp(liste[i].espece, espece) == 0)
         {
             afficherAnimal(liste[i]);
+            trouve += 1;
         }
     }
-    return 0;
+    return trouve;
 }
 
 int chercheAge(Animal liste[], int taille, char *age)
 {
+    int trouve = 0;
     for (int i = 0; i < taille; i++)
     {
         if (strcmp(age, "jeune") == 0)
@@ -36,16 +42,19 @@ int chercheAge(Animal liste[], int taille, char *age)
             if (calculerAge(liste[i].anneNaissance) < 2)
             {
                 afficherAnimal(liste[i]);
+                trouve += 1;
             }
+
         }
         if (strcmp(age, "senior") == 0)
         {
             if (calculerAge(liste[i].anneNaissance) > 10)
             {
                 afficherAnimal(liste[i]);
+                trouve += 1;
             }
         }
     }
 
-    return 0;
+    return trouve;
 }
